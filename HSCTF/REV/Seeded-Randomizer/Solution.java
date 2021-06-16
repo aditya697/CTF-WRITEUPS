981d3738854a28b36988ebe16a840f77bedd9fc7
aditya697 <73250884+aditya697@users.noreply.github.com>
2021-06-16 20:57:05 +0530

    Create Solution.java

---
+++ HSCTF/REV/Seeded-Randomizer/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class SeededRandomizer {
+
+	public static void display(char[] arr) {
+		for (char x: arr)
+			System.out.print(x);
+		System.out.println();
+	}
+
+	public static void sample() {
+		Random rand = new Random(79808677);
+		char[] test = new char[12];
+		int[] b = {9, 3, 4, -1, 62, 26, -37, 75, 83, 11, 30, 3};
+		for (int i = 0; i < test.length; i++) {
+			int n = rand.nextInt(128) + b[i];
+			test[i] = (char)n;
+		}
+		display(test);
+	}
+
+	public static void main(String[] args) {
+        for(int x=0; x<1001 ; x++){
+            Random rand = new Random(x);
+            char[] flag = new char[33];
+            int[] c = {13, 35, 15, -18, 88, 68, -72, -51, 73, -10, 63, 
+                    1, 35, -47, 6, -18, 10, 20, -31, 100, -48, 33, -12, 
+                    13, -24, 11, 20, -16, -10, -76, -63, -18, 118};
+            for (int i = 0; i < flag.length; i++) {
+                int n = rand.nextInt(128) + c[i];
+                flag[i] = (char)n;
+            }
+            display(flag);
+        }
+    }
+}
