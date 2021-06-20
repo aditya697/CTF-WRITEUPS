a42bc53121df729c69c259a07376b0e33d9b2491
aditya697 <73250884+aditya697@users.noreply.github.com>
2021-06-20 11:13:44 +0530

    Update Solution.py

--- HSCTF/Algo/LUNCH_WITH_CIA/Solution.py
+++ HSCTF/Algo/LUNCH_WITH_CIA/Solution.py
@@ -25,3 +25,39 @@ for i in range(n):
 
 result = {}
 print(get_waste(l,w,lst))
+
+#AUTOMATED CODE
+from pwn import *
+
+p = remote("lunch-with-the-cia.hsc.tf",1337)
+
+result = {}
+
+def get_waste(l, w, a):
+    if (l, w) in result:
+        return result[(l,w)]
+    minimum = l*w
+    for cake in a:
+        x = cake[0]
+        y = cake[1]
+        if (x <= l and y <= w):
+            minimum = min(minimum, (get_waste(l-x, w, a) + get_waste(x, w-y, a)))
+            minimum = min(minimum, (get_waste(l, w-y, a) + get_waste(l-x, y, a)))
+    result[(l,w)] = minimum
+    return minimum
+
+for i in range(10):
+    p.recvuntil("Test case ")
+    print(p.recvline())
+    l, w, n = map(int, p.recvline().split())
+    lst = []
+    for _ in range(n):
+        lst.append(tuple(map(int, p.recvline().split())))
+    result = {}
+    print(l, w, lst)
+    res = get_waste(l, w, lst)
+    print(res)
+    p.sendline(str(res))
+    print(p.recvline())
+while True:
+    print(p.recvline())
